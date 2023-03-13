c8b739fd020ab3e3689b7e87e4c0f757ba21cebe
IamNotJakeHill <quba5928@gmail.com>
2023-03-13 13:07:48 +0100

    Poprawny plik z 3 zajęć

--- preprocessing.py
+++ preprocessing.py
@@ -1,95 +1,146 @@
++
 import numpy as np
-#  import github
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from pip._internal.vcs import git
 from sklearn.preprocessing import LabelEncoder
 import math
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 
-#zaczytaj dane z pliku csv
-df_train = pd.read_csv("train.csv" , sep = "," , encoding = 'utf-8', low_memory=False )
-#sprawdź liczbę kolumn i wierszy
-df_train.shape
-df_train.info()
-#wyświetl część tabeli
+# zaczytanie danych z pliku
+df_train = pd.read_csv("train.csv", sep=",", encoding='utf-8')
+# sprawdzenie rozmiaru
+print(df_train.shape)
+# sprawdzenie nazw kolumn i ich typów
+print(df_train.info())
+# wyświetl część tabeli
 df_train.head()
-#usuń wiersze z duplikatami id
+# usunięcie wierszy z duplikatami id
 df_train.drop_duplicates(subset="ID", inplace=True)
+list_of_features = [
+    'Month',
+    'Age',
+    'Occupation',
+    'Annual_Income',
+    'Monthly_Inhand_Salary',
+    'Num_Bank_Accounts',
+    'Num_Credit_Card',
+    'Interest_Rate',
+    'Num_of_Loan',
+    'Delay_from_due_date',
+    'Num_of_Delayed_Payment',
+    'Changed_Credit_Limit',
+    'Num_Credit_Inquiries',
+    'Credit_Mix',
+    'Outstanding_Debt',
+    'Credit_Utilization_Ratio',
+    'Credit_History_Age',
+    'Payment_of_Min_Amount',
+    'Total_EMI_per_month',
+    'Amount_invested_monthly',
+    'Payment_Behaviour',
+    'Monthly_Balance', ]
 
-#opisz statystyki danych
-df_train.describe()
-#zlicz różne wartości danych
+# nazwy kolumn dla danych docelowo numerycznych
+FeaturesToConvert = ['Age', 'Annual_Income', 'Num_of_Loan', 'Num_of_Delayed_Payment',
+                     'Changed_Credit_Limit', 'Outstanding_Debt', 'Amount_invested_monthly',
+                     'Monthly_Balance']
+
+# nazwy kolumn dla danych kategorycznych
+
+# liczności danych kategorii
+# sns.countplot(df_train['Credit_Score'])
+
+# sprawdzenie danych
 for i in df_train.columns:
     print(df_train[i].value_counts())
-    print('*'*50)
-    # pokaż liczności danych kategorycznych
+    print('*' * 50)
 
-#zastąp błędne dane
+# wyświetl liczbę pustych wartosci
+print(df_train.isnull().sum().sort_values(ascending=False))
 
-
-df_train.info()
-#zmień dane na numeryczne
-FeaturesToConvert = ['Age', 'Annual_Income',
-'Num_of_Loan', 'Num_of_Delayed_Payment',
-'Changed_Credit_Limit', 'Outstanding_Debt',
-'Amount_invested_monthly', 'Monthly_Balance']
-# ale najpierw sprawdź czy nie ma błędów w danych
+########## dane numeryczne #########################
+# usuń zbędne znaki '-’ , '_'
 for feature in FeaturesToConvert:
-    uniques = df_train[feature].unique()
-    print('Feature:', '\n', feature, '\n', uniques, '\n', '--'*40, '\n')
-    # usuń zbędne znaki '-’ , '_'
-    for feature in FeaturesToConvert:
-        df_train[feature]=str(df_train[feature])
-        df_train[feature] = df_train[feature].str.strip('-_');
-        # puste kolumny zastąp NAN
-    for feature in FeaturesToConvert:
-        df_train[feature] =df_train[feature].replace({'':np.nan});
-        # zmien typ zmiennych ilościowych
-    for feature in FeaturesToConvert:
-        df_train[feature] = df_train[feature].astype('float64');
+    df_train[feature] = df_train[feature].str.strip('-_')
+# puste kolumny zastąp NAN 
+for feature in FeaturesToConvert:
+    df_train[feature] = df_train[feature].replace({'': np.nan})
+# zmien typ zmiennych ilościowych
+for feature in FeaturesToConvert:
+    df_train[feature] = df_train[feature].astype('float64')
 
-df_train['Monthly_Inhand_Salary']= df_train['Monthly_Inhand_Salary'].fillna(method='pad')
+# uzupełnij braki średnią
+df_train['Monthly_Inhand_Salary'] = df_train['Monthly_Inhand_Salary'].fillna(method='pad')
+df_train['Monthly_Balance'] = df_train['Monthly_Balance'].fillna(method='pad')
+df_train['Type_of_Loan'] = df_train['Type_of_Loan'].fillna(method='ffill')
+df_train['Credit_History_Age'] = df_train['Credit_History_Age'].fillna(method='pad')
+df_train['Num_of_Delayed_Payment'] = df_train['Num_of_Delayed_Payment'].fillna(method='pad')
+df_train['Amount_invested_monthly'] = df_train['Amount_invested_monthly'].fillna(method='pad')
+df_train['Changed_Credit_Limit'] = df_train['Changed_Credit_Limit'].fillna(method='pad')
+df_train['Num_Credit_Inquiries'] = df_train['Num_Credit_Inquiries'].fillna(method='pad')
 
-from sklearn.preprocessing import LabelEncoder
+print(df_train.isnull().sum().sort_values(ascending=False))
+
+# zastąpienie nierealnych wartości medianą
+for i in df_train.Age.values:
+    if (i > 118 or i < 0):
+        df_train.Age.replace(i, np.median(df_train.Age), inplace=True)
+
+############ zmienne kategoryczne #####################
 # stwórz obiekt enkodera
 le = LabelEncoder()
-df_train.Occupation = le.fit_transform(df_train.Occupation)
-# sprawdź transformacje
-df_train.head()
 
+CatFeatures = ['Occupation', 'Credit_Mix', 'Payment_of_Min_Amount',
+               'Payment_Behaviour', 'Credit_Score', 'Month']
 
-cols = ['workex', 'status', 'hsc_s', 'degree_t']
-# Encode labels of multiple columns at once
-df_train[cols] = df_train[cols].apply(LabelEncoder().fit_transform)
-# Print head
-df_train.head()
+# zakoduj etykiety słowne numerycznymi
 
+df_train[CatFeatures] = df_train[CatFeatures].apply(LabelEncoder().fit_transform)
 df_train['Credit_History_Age'].head()
+# podziel kolumnę po tekście i przelicz na
+# miesiące
+df_train['Credit_History_Age'] = 12 * (df_train["Credit_History_Age"].str.split(" ", expand=True))[0] + (
+df_train["Credit_History_Age"].str.split(" ", expand=True)[3])
+df_train['Credit_History_Age'] = df_train['Credit_History_Age'].astype(float)
+# usuń nieistotne kolumny
 
-plt.figure(figsize = (20, 18))
-sns.heatmap(df_train['Credit_History_Age'].corr(), annot =True, linewidths = 0.1, cmap = 'Blues')
+irrelevant = ['ID', 'Customer_ID', 'Name', 'SSN', 'Type_of_Loan']
+df_train = df_train.drop(irrelevant, axis=1)
+print("l 85 CHECKCHECK")
+#######################################################
+# sprawdź transformacje
+print(df_train.shape)
+print('\n**********************************\n')
+print(df_train.info())
+print('\n**********************************\n')
+print(df_train.describe().transpose())
+print('\n**********************************\n')
+######################################################
+
+Q1 = df_train.Annual_Income.quantile(0.25)
+Q3 = df_train.Annual_Income.quantile(0.75)
+IQR = Q3 - Q1
+df = df_train.drop(df_train.loc[df_train['Annual_Income'] > (Q3 + 1.5 *
+                                                             IQR)].index)
+df = df_train.drop(df.loc[df_train['Annual_Income'] < (Q1 - 1.5 * IQR)].index)
+
+# różne sposoby https://scikit-learn.org/stable/modules/preprocessing.html#standardization-or-mean-removal-and-variance-scaling
+scaler = MinMaxScaler()  # scaler = StandardScaler()
+col_float = ['Age', 'Annual_Income', 'Delay_from_due_date',
+             'Num_of_Delayed_Payment', 'Outstanding_Debt', 'Credit_History_Age',
+             'Total_EMI_per_month', 'Monthly_Balance']
+for i in df[col_float]:
+    df_train[i] = scaler.fit_transform(df[[i]])
+print(df.head())
+
+#  OPCJONALNE
+plt.figure(figsize=(20, 18))
+sns.heatmap(df_train[list_of_features].corr(), annot=True, linewidths=0.1, cmap='Blues')
 plt.title('Numerical Features Correlation')
 plt.show()
 
-scaler = MinMaxScaler()
-col_float = ['Age', 'Annual_Income',
-'Delay_from_due_date', 'Num_of_Delayed_Payment',
-'Outstanding_Debt', 'Credit_History_Age',
-'Total_EMI_per_month', 'Monthly_Balance']
-df_cleaned = []
-for i in col_float:
-    df_cleaned[i] =scaler.fit_transform(df_cleaned[[i]])
-    df_cleaned.head()
+# WYRZUCIC 'Outstanding_Debt' i 'Payment_of_Min_Amount'
 
-Q1 = df_cleaned.Annual_Income.quantile(0.25)
-Q3 = df_cleaned.Annual_Income.quantile(0.75)
-IQR = Q3 - Q1
-df_cleaned =df_cleaned.drop(df_cleaned.loc[df_cleaned['Annual_Income'] > (Q3 + 1.5 * IQR)].index)
-df_cleaned =df_cleaned.drop(df_cleaned.loc[df_cleaned['Annual_Income'] < (Q1 - 1.5 * IQR)].index)
-
-from sklearn.model_selection import train_test_split
-    # git.commit-m "przygotowanie danych"
-    # git.push
+df_train.drop(['Outstanding_Debt', 'Payment_of_Min_Amount'], axis=1)
