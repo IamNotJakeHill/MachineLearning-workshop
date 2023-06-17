c8ef5c585c17309af3a5bc1bcd83db74d32a252d
IamNotJakeHill <quba5928@gmail.com>
2023-06-17 17:22:19 +0200

    PCA PASTE FROM MAIN

--- PCA.py
+++ PCA.py
@@ -1,7 +1,10 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.linear_model import LogisticRegression
+import matplotlib
 # Zaczytanie danych z pliku
 df = pd.read_csv("cleaned_data.csv", sep=",", encoding='utf-8', index_col= 0)
 frame = df.drop(['Credit_Score'], axis = 1)
@@ -32,7 +35,21 @@ print(glowneFrame.shape[1])
 # sprawdzenie nazw kolumn i ich typów
 print(glowneFrame.info())
 
-x_train, x_test, y_train, y_test = train_test_split(glowneFrame, creditScore, test_size= 0.5, random_state= 69)
+x_train, x_test, y_train, y_test = train_test_split(glowneFrame, creditScore, test_size= 0.5, random_state= 100)
+
+clf = LogisticRegression(random_state=100)
+clf.fit(x_train, y_train)
+clf.predict(x_test)
+
+matrix = confusion_matrix(y_test,y_train)
+ConfusionMatrixDisplay(matrix).plot()
+plt.show()
+raport = classification_report(y_test,y_train)
+print(raport)
+
+LogisticRegression(random_state=100)
+
+
 
 print(x_train)
 print(x_test)
